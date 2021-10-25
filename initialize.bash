a26bdd75d686fe5f3421177baf27663ced1d1906
Hassan Afrouzi <hassan@afrouzi.com>
2021-10-24 21:39:34 -0400

    update initialize.bash

--- initialize.bash
+++ initialize.bash
@@ -1,6 +1,7 @@
 # This file changes the parent folder name to your project's name and initializes a git repository in the folder
-# To run the file: add project name after the equal sign and run the following command in terminal: 'source ./initialize.bash'
-PROJECT_NAME=
+# To run the file: enter the following command in terminal: 'source ./initialize.bash'
+echo "Enter your project name:"
+read PROJECT_NAME
 
 cd ..
 mv tex_template $PROJECT_NAME
